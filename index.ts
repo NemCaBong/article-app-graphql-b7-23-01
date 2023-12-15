65a2c6a9fc0e85a6173f56beee2bdc598c86e377
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-12-15 21:41:58 +0700

    5. Tạo route private

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { ApolloServer } from "apollo-server-express";
 
 import { typeDefs } from "./typeDefs/index.typeDefs";
 import { resolvers } from "./resolvers/index.resolver";
+import { requireAuth } from "./middlewares/auth.middleware";
 
 const startServer = async () => {
   dotenv.config();
@@ -15,9 +16,12 @@ const startServer = async () => {
   const port: number | string = process.env.PORT || 3000;
   
   // Graphql
+  app.use("/graphql", requireAuth);
+
   const apolloServer = new ApolloServer({
     typeDefs: typeDefs,
-    resolvers: resolvers
+    resolvers: resolvers,
+    context: ({ req }) => req,
   });
   
   await apolloServer.start();
